83ca025f1065d23b0085e4ff056093a37f370f90
ngrieger <ngrieger@KO09C-PRAK26.scp.koeln.fh-aachen.de>
2017-06-22 12:08:36 +0200

    ki dim 2

--- src/main/java/ki/GreedyKi.java
+++ src/main/java/ki/GreedyKi.java
@@ -45,12 +45,24 @@ public class GreedyKi extends KI {
 				for (Point possibleMove : possibleMoves(tempPlayerPos, newBoard)) {
 					int distance = getDistance(tempTreasurePos, possibleMove);
 					if (distance < minDistance) {
-						minDistance = distance;
-						bestMove = possibleMove;
-						bestShiftCard = shiftCard;
-						bestShiftPos = shiftPoint;
+						// minDistance = distance;
+						// bestMove = possibleMove;
+						// bestShiftCard = shiftCard;
+						// bestShiftPos = shiftPoint;
 						if (distance == 0) {
 							break outer;
+						} else {
+							Point tempForbidden = new Point(shiftPoint);
+							if (tempForbidden.x == 0 || tempForbidden.x == 6)
+								tempForbidden.x = Math.abs(tempForbidden.x - 6);
+							if (tempForbidden.y == 0 || tempForbidden.y == 6)
+								tempForbidden.y = Math.abs(tempForbidden.y - 6);
+							if (moveDim2(tempTreasurePos, tempPlayerPos, tempForbidden, newBoard)) {
+								minDistance = distance;
+								bestMove = possibleMove;
+								bestShiftCard = shiftCard;
+								bestShiftPos = shiftPoint;
+							}
 						}
 					}
 				}
@@ -60,6 +72,44 @@ public class GreedyKi extends KI {
 		}
 
 		client.move(bestMove.x, bestMove.y, bestShiftPos.x, bestShiftPos.y, bestShiftCard);
+	}
+
+	public boolean moveDim2(Point treasurePos, Point playerPos, Point forbiddenPos, Board board) {
+		// art der karte -> anzahl drehungen
+		int rotateCount = isCardIShape(data.card) ? 1 : 3;
+
+		// Karte reinschieben
+
+		CardType shiftCard = data.card;
+		for (int i = 0; i < POSTOSHIFTCARD.length; i++) {
+			Point shiftPoint = new Point(POSTOSHIFTCARD[i][0], POSTOSHIFTCARD[i][1]);
+			if (shiftPoint.equals(data.forbiddenPos))
+				continue;
+			for (int j = 0; j < rotateCount; j++) {
+				Point tempPlayerPos = (Point) playerPos.clone();
+				Point tempTreasurePos = (Point) treasurePos.clone();
+				Board newBoard = new Board(data.board);
+				boolean vertical = shiftPoint.y == 0 || shiftPoint.y == 6;
+				boolean rightTop = shiftPoint.y == 0 || shiftPoint.x == 6;
+				Point[] tempPos = newBoard.placeShiftCard(shiftCard, shiftPoint, tempPlayerPos, tempTreasurePos,
+						vertical, rightTop);
+				tempPlayerPos = tempPos[0];
+				tempTreasurePos = tempPos[1];
+				if (tempTreasurePos.x < 0 || tempTreasurePos.x > 6 || tempTreasurePos.y < 0 || tempTreasurePos.y > 6)
+					break;
+
+				for (Point possibleMove : possibleMoves(tempPlayerPos, newBoard)) {
+					int distance = getDistance(tempTreasurePos, possibleMove);
+					if (distance == 0) {
+						return true;
+					}
+				}
+
+				shiftCard = rotateCard(shiftCard);
+			}
+		}
+
+		return false;
 	}
 
 	private int getDistance(Point from, Point to) {
