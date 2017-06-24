fefa6b48b0674dc41cadd7273f055ed7b488f5dd
Niklas Grieger <niklas.grieger@alumni.fh-aachen.de>
2017-06-24 19:48:17 +0200

    Replace PointWeightPair.java

--- src/main/java/ki/PointWeightPair.java
+++ src/main/java/ki/PointWeightPair.java
@@ -1,9 +1,8 @@
 package ki;
 
 import java.awt.Point;
-import java.util.Comparator;
 
-public class PointWeightPair implements Comparator<PointWeightPair> {
+public class PointWeightPair implements Comparable<PointWeightPair> {
 	public Point point;
 	public int weight;
 
@@ -13,11 +12,37 @@ public class PointWeightPair implements Comparator<PointWeightPair> {
 	}
 
 	@Override
-	public int compare(PointWeightPair o1, PointWeightPair o2) {
-		return Integer.compare(o1.weight, o2.weight);
+	public int compareTo(PointWeightPair o) {
+		return Integer.compare(weight, o.weight);
 	}
 
-	public boolean equals(Object o) {
-		return point.equals(((PointWeightPair) o).point);
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((point == null) ? 0 : point.hashCode());
+		result = prime * result + weight;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PointWeightPair other = (PointWeightPair) obj;
+		if (point == null) {
+			if (other.point != null)
+				return false;
+		} else if (!point.equals(other.point))
+			return false;
+		return true;
+	}
+
+	public String toString() {
+		return point.x + "/" + point.y + ":" + weight + "\n";
 	}
 }
